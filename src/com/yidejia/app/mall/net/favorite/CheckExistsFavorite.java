2b91dd65a17b0fed440dc5f3f82b50d6705fcf8f
longbin <675754509@qq.com>
2013-09-30 16:20:38 +0800

    添加了检查是否收藏了某商品

---
+++ src/com/yidejia/app/mall/net/favorite/CheckExistsFavorite.java
@@ -0,0 +1,92 @@
+package com.yidejia.app.mall.net.favorite;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.http.conn.ConnectTimeoutException;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.yidejia.app.mall.net.HttpAddressParam;
+import com.yidejia.app.mall.net.HttpGetConn;
+import com.yidejia.app.mall.net.HttpPostConn;
+import com.yidejia.app.mall.util.Md5;
+/**
+ * 保存（新建,更新）收藏
+ * @author long bin
+ *
+ */
+public class CheckExistsFavorite {
+	private String[] keys = new String[7];
+	private String[] values = new String[7];
+	private String TAG = "CheckExistsFavorite";
+	private Context context;
+	private Map<String, String> httpAddressMap = new ConcurrentHashMap<String, String>();
+	
+	public CheckExistsFavorite(Context context){
+		this.context = context;
+	}
+	
+	private void setKeysAndValues(String userid, String goodsid){
+		keys[0] = "api";
+		String api = "product.favoliten.checkExists";
+		values[0] = api;
+		keys[1] = "userId";
+		values[1] = userid;
+		keys[2] = "goods_id";
+		values[2] = goodsid;
+		
+		keys[3] = "key";
+		values[3] = "fw_mobile";
+		keys[4] = "format";
+		values[4] = "array";
+		keys[5] = "ts";
+		long time = System.currentTimeMillis();
+		String ts = String.valueOf(time/1000);
+		values[5] = ts;
+		
+		keys[6] = "sign";
+		StringBuffer strTemp = new StringBuffer();
+		strTemp.append("ChunTianfw_mobile123456");
+		strTemp.append(api);
+		strTemp.append(ts);
+		Md5 md = new Md5();
+		String result = md.getMD5Str(strTemp.toString());
+		md = null;
+        strTemp = null;
+		values[6] = result;
+	}
+	
+	private String getHttpAddress(String userid, String goodsid){
+		StringBuffer result = new StringBuffer();
+		result.append("http://192.168.1.254:802/?");
+		setKeysAndValues(userid, goodsid);
+		result.append(HttpAddressParam.getHttpAddress(keys, values));
+		Log.i(TAG, result.toString());
+		for (int j = 0; j < keys.length; j++) {
+			httpAddressMap.put(keys[j], values[j]);
+		}
+		return result.toString();
+	}
+	
+	private String result = "";
+	private String urlString = "http://192.168.1.254:802/";
+	/**
+	 * 
+	 * @param userid 用户id
+	 * @param goodsid 收藏的商品id
+	 * @return http response string
+	 * @throws IOException
+	 */
+	public String httpResponse(String userid, String goodsid) throws IOException{
+//		getHttpAddress(userid, goodsid);
+		HttpGetConn conn = new HttpGetConn(getHttpAddress(userid, goodsid));
+		result = conn.getJsonResult();
+		return result;
+	}
+	
+}
