e8673ceb3afceb240f05391bfda2c5a8b39c3ef8
longbin <675754509@qq.com>
2013-10-09 17:38:27 +0800

    搜索历史模块，积分，收藏

---
+++ src/com/yidejia/app/mall/datamanage/VoucherDataManage.java
@@ -0,0 +1,18 @@
+package com.yidejia.app.mall.datamanage;
+
+/**
+ * 获取用户积分
+ * @author long bin
+ *
+ */
+public class VoucherDataManage {
+	/**
+	 * 获取用户积分
+	 * @param userid 用户id
+	 * @return
+	 */
+	public int getUserVoucher(String userid){
+		int voucher = -1;
+		return voucher;
+	};
+}
