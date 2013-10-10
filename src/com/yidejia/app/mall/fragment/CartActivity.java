a4b3a48438954d28e29f701880b5ecbdcbea969e
liuyong <liyong@qq.com>
2013-10-10 15:44:29 +0800

    付款时，判断地址是存在，和是否有商品

--- src/com/yidejia/app/mall/fragment/CartActivity.java
+++ src/com/yidejia/app/mall/fragment/CartActivity.java
@@ -26,11 +26,16 @@ import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.yidejia.app.mall.GoodsInfoActivity;
+import com.yidejia.app.mall.MyApplication;
 import com.yidejia.app.mall.R;
+import com.yidejia.app.mall.datamanage.AddressDataManage;
 import com.yidejia.app.mall.datamanage.CartsDataManage;
+import com.yidejia.app.mall.model.Addresses;
 import com.yidejia.app.mall.model.UserComment;
 import com.yidejia.app.mall.util.CartUtil;
 import com.yidejia.app.mall.util.Consts;
+import com.yidejia.app.mall.view.GoCartActivity;
+import com.yidejia.app.mall.view.NewAddressActivity;
 import com.yidejia.app.mall.view.PayActivity;
 
 public class CartActivity extends SherlockFragment implements OnClickListener {
@@ -53,14 +58,15 @@ public class CartActivity extends SherlockFragment implements OnClickListener {
 	private ArrayList<UserComment> mlist;
 	private CartUtil cartUtil;
 	private CheckBox mBox;
-	private final int MENU1  = 0x111;
-	private final int MENU2  = 0x112;
-	private final int MENU3  = 0x113;
+	private final int MENU1 = 0x111;
+	private final int MENU2 = 0x112;
+	private final int MENU3 = 0x113;
 	private LinearLayout layout;
 	private View view;
 	private CartsDataManage dataManage2;
-//	private InnerReceiver receiver;
-	
+	private AddressDataManage addressManage;// 地址管理
+
+	// private InnerReceiver receiver;
 
 	// private void doClick(View v) {
 	// switch (v.getId()) {
@@ -183,223 +189,257 @@ public class CartActivity extends SherlockFragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-	
+		addressManage = new AddressDataManage(getSherlockActivity());
 		view = inflater.inflate(R.layout.shopping_cart, container, false);
-//			dataManage = new CartsDataManage();
-			// TODO Auto-generated method stub
+		// dataManage = new CartsDataManage();
+		// TODO Auto-generated method stub
 
-			mBox = (CheckBox) view.findViewById(R.id.shopping_cart_checkbox);// 选择框
+		mBox = (CheckBox) view.findViewById(R.id.shopping_cart_checkbox);// 选择框
 
-			sumTextView = (TextView) view
-					.findViewById(R.id.shopping_cart_sum_money);// 总的钱数
+		sumTextView = (TextView) view
+				.findViewById(R.id.shopping_cart_sum_money);// 总的钱数
 
-			counTextView = (TextView) view
-					.findViewById(R.id.shopping_cart_sum_number);// 总的数量
+		counTextView = (TextView) view
+				.findViewById(R.id.shopping_cart_sum_number);// 总的数量
 
-			 layout = (LinearLayout) view
-					.findViewById(R.id.shopping_cart_relative2);
-			
-//			registerForContextMenu(layout);
+		layout = (LinearLayout) view.findViewById(R.id.shopping_cart_relative2);
 
-			getSherlockActivity().getSupportActionBar().setCustomView(
-					R.layout.actionbar_cart);
-			cartUtil = new CartUtil(getSherlockActivity(), layout, counTextView,
-					sumTextView,mBox);
-			
-			cartUtil.AllComment();
-			
-//			receiver = new InnerReceiver();
-//			IntentFilter filter = new IntentFilter();
-//			filter.addAction(Consts.UPDATE_CHANGE);
-//			getActivity().registerReceiver(receiver, filter);
+		// registerForContextMenu(layout);
 
-	
-			
-			
-			// List<HashMap<String, Integer>> mlist = cartUtil.list;
-			// for(int i = 0;i<mlist.size();i++){
-			// HashMap<String, Integer> map = mlist.get(i);
-			// int count = map.get("count");
-			// Log.i("info", count+"");
-			//
-			// Set<java.util.Map.Entry<String, Integer>> set = map.entrySet();
-			// for(java.util.Map.Entry<String, Integer> en:set){
+		getSherlockActivity().getSupportActionBar().setCustomView(
+				R.layout.actionbar_cart);
+		cartUtil = new CartUtil(getSherlockActivity(), layout, counTextView,
+				sumTextView, mBox);
 
-			// }
-			// }
+		cartUtil.AllComment();
 
-			// ListView listView = (ListView)
-			// view.findViewById(R.id.shopping_cart_listview);
-			// dataManage = new UserCommentDataManage(getSherlockActivity());
-			//
-			// mlist = dataManage.getUserCommentsArray(26 + "", 0, 10, false);
-			// CartAdapter adapter = new CartAdapter(getSherlockActivity(),mlist);
-			// listView.setAdapter(adapter);
-			// listView.setOnItemClickListener(new OnItemClickListener() {
-			//
-			// @Override
-			// public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-			// long arg3) {
-			// // TODO Auto-generated method stub
-			//
-			// }
-			// });
+		// receiver = new InnerReceiver();
+		// IntentFilter filter = new IntentFilter();
+		// filter.addAction(Consts.UPDATE_CHANGE);
+		// getActivity().registerReceiver(receiver, filter);
 
-			// person = inflater.inflate(R.layout.shopping_cart_item, null);
-			// person2 = inflater.inflate(R.layout.shopping_cart_item, null);
-			//
-			//
-			// TextView detail = (TextView) person
-			// .findViewById(R.id.shopping_cart_item_text);
-			//
-			// detail.setOnClickListener(new OnClickListener() {
-			//
-//			 @Override
-//			 public void onClick(View arg0) {
-//			 // TODO Auto-generated method stub
-//			 Intent intent = new Intent(getSherlockActivity(),
-//			 OrderDetailActivity.class);
-//			 startActivity(intent);
-//			 }
-//			 });
-			// subtract = (ImageView) person
-			// .findViewById(R.id.shopping_cart_item_subtract);// 减
-			// number = (TextView) person
-			// .findViewById(R.id.shopping_cart_item_edit_number);// 输入的个数
-			// number.setText(0 + "");
-			// addImageView = (ImageView) person
-			// .findViewById(R.id.shopping_cart_item_add);// 加
-			// priceTextView1 = (TextView) person
-			// .findViewById(R.id.shopping_cart_item_money);
-			//
-			// addImageView.setOnClickListener(this);
-			// subtract.setOnClickListener(this);
-			//
-			// TextView detail1 = (TextView) person2
-			// .findViewById(R.id.shopping_cart_item_text);
-			//
-			// detail1.setOnClickListener(new OnClickListener() {
-			//
-			// @Override
-			// public void onClick(View arg0) {
-			// // TODO Auto-generated method stub
-			// Intent intent = new Intent(getSherlockActivity(),
-			// OrderDetailActivity.class);
-			// startActivity(intent);
-			// }
-			// });
-			//
-			//
-			//
-			//
-			//
-			// subtract2 = (ImageView) person2
-			// .findViewById(R.id.shopping_cart_item_subtract);// 减
-			// number2 = (TextView) person2
-			// .findViewById(R.id.shopping_cart_item_edit_number);// 输入的个数
-			// number2.setText(0 + "");
-			// addImageView2 = (ImageView) person2
-			// .findViewById(R.id.shopping_cart_item_add);// 加
-			// priceTextView2 = (TextView) person2
-			// .findViewById(R.id.shopping_cart_item_money);
-			// addImageView2.setOnClickListener(this);
-			// subtract2.setOnClickListener(this);
-			//
-			// layout.addView(person);
-			// layout.addView(person2);
+		// List<HashMap<String, Integer>> mlist = cartUtil.list;
+		// for(int i = 0;i<mlist.size();i++){
+		// HashMap<String, Integer> map = mlist.get(i);
+		// int count = map.get("count");
+		// Log.i("info", count+"");
+		//
+		// Set<java.util.Map.Entry<String, Integer>> set = map.entrySet();
+		// for(java.util.Map.Entry<String, Integer> en:set){
 
-			// 结算
-			shoppingCartTopay = (Button) getSherlockActivity().findViewById(
-					R.id.shopping_cart_go_pay);
-			shoppingCartTopay.setOnClickListener(new OnClickListener() {
+		// }
+		// }
 
-				@Override
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-					Intent intent = new Intent(getSherlockActivity(),
-							PayActivity.class);
-					Bundle bundle = new Bundle();
-					bundle.putString("price", sumTextView.getText().toString());
-					intent.putExtras(bundle);
-					getSherlockActivity().startActivity(intent);
+		// ListView listView = (ListView)
+		// view.findViewById(R.id.shopping_cart_listview);
+		// dataManage = new UserCommentDataManage(getSherlockActivity());
+		//
+		// mlist = dataManage.getUserCommentsArray(26 + "", 0, 10, false);
+		// CartAdapter adapter = new CartAdapter(getSherlockActivity(),mlist);
+		// listView.setAdapter(adapter);
+		// listView.setOnItemClickListener(new OnItemClickListener() {
+		//
+		// @Override
+		// public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+		// long arg3) {
+		// // TODO Auto-generated method stub
+		//
+		// }
+		// });
+
+		// person = inflater.inflate(R.layout.shopping_cart_item, null);
+		// person2 = inflater.inflate(R.layout.shopping_cart_item, null);
+		//
+		//
+		// TextView detail = (TextView) person
+		// .findViewById(R.id.shopping_cart_item_text);
+		//
+		// detail.setOnClickListener(new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View arg0) {
+		// // TODO Auto-generated method stub
+		// Intent intent = new Intent(getSherlockActivity(),
+		// OrderDetailActivity.class);
+		// startActivity(intent);
+		// }
+		// });
+		// subtract = (ImageView) person
+		// .findViewById(R.id.shopping_cart_item_subtract);// 减
+		// number = (TextView) person
+		// .findViewById(R.id.shopping_cart_item_edit_number);// 输入的个数
+		// number.setText(0 + "");
+		// addImageView = (ImageView) person
+		// .findViewById(R.id.shopping_cart_item_add);// 加
+		// priceTextView1 = (TextView) person
+		// .findViewById(R.id.shopping_cart_item_money);
+		//
+		// addImageView.setOnClickListener(this);
+		// subtract.setOnClickListener(this);
+		//
+		// TextView detail1 = (TextView) person2
+		// .findViewById(R.id.shopping_cart_item_text);
+		//
+		// detail1.setOnClickListener(new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View arg0) {
+		// // TODO Auto-generated method stub
+		// Intent intent = new Intent(getSherlockActivity(),
+		// OrderDetailActivity.class);
+		// startActivity(intent);
+		// }
+		// });
+		//
+		//
+		//
+		//
+		//
+		// subtract2 = (ImageView) person2
+		// .findViewById(R.id.shopping_cart_item_subtract);// 减
+		// number2 = (TextView) person2
+		// .findViewById(R.id.shopping_cart_item_edit_number);// 输入的个数
+		// number2.setText(0 + "");
+		// addImageView2 = (ImageView) person2
+		// .findViewById(R.id.shopping_cart_item_add);// 加
+		// priceTextView2 = (TextView) person2
+		// .findViewById(R.id.shopping_cart_item_money);
+		// addImageView2.setOnClickListener(this);
+		// subtract2.setOnClickListener(this);
+		//
+		// layout.addView(person);
+		// layout.addView(person2);
+
+		// 结算
+		shoppingCartTopay = (Button) getSherlockActivity().findViewById(
+				R.id.shopping_cart_go_pay);
+		shoppingCartTopay.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				getAddresses();
+				Intent intent = new Intent(getSherlockActivity(),
+						PayActivity.class);
+				Bundle bundle = new Bundle();
+				float sum = Float.parseFloat(sumTextView.getText().toString());
+				if (address == null) {
+					Toast.makeText(getSherlockActivity(), "您还未填写收货人地址",
+							Toast.LENGTH_LONG).show();
+
+				} else {
+					if (sum > 0) {
+						bundle.putString("price", sum + "");
+						bundle.putSerializable("address", address);
+						intent.putExtras(bundle);
+						getSherlockActivity().startActivity(intent);
+					} else {
+						Toast.makeText(getSherlockActivity(), "你还未购买任何商品",
+								Toast.LENGTH_LONG).show();
+
+					}
 				}
-			});
-			//
-			return view;
-	
-		
+			}
+		});
+		//
+		return view;
 
 	}
-	
-	
-//	@Override
-//	public void onCreateContextMenu(ContextMenu menu, View v,
-//			ContextMenuInfo menuInfo) {
-//		menu.add(0, MENU1, 0, "删除");
-//		menu.add(0, MENU2, 0, "查看商品详情");
-//		menu.add(0, MENU3, 0, "收藏");
-//		menu.setGroupCheckable(0, true, true);
-//		menu.setHeaderIcon(android.R.drawable.menu_frame);
-//		menu.setHeaderTitle("购物车操作");
-//		super.onCreateContextMenu(menu, v, menuInfo);
-//	}
 
-//	@Override
-//	public void onDestroy() {
-//		getActivity().unregisterReceiver(receiver);
-//		
-//		super.onDestroy();
-//	}
+	// @Override
+	// public void onCreateContextMenu(ContextMenu menu, View v,
+	// ContextMenuInfo menuInfo) {
+	// menu.add(0, MENU1, 0, "删除");
+	// menu.add(0, MENU2, 0, "查看商品详情");
+	// menu.add(0, MENU3, 0, "收藏");
+	// menu.setGroupCheckable(0, true, true);
+	// menu.setHeaderIcon(android.R.drawable.menu_frame);
+	// menu.setHeaderTitle("购物车操作");
+	// super.onCreateContextMenu(menu, v, menuInfo);
+	// }
 
+	// @Override
+	// public void onDestroy() {
+	// getActivity().unregisterReceiver(receiver);
+	//
+	// super.onDestroy();
+	// }
 
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
 
 	}
-//	//接收广播
-//	private class InnerReceiver extends BroadcastReceiver{
-//
-//		@Override
-//		public void onReceive(Context context, Intent intent) {
-//			// TODO Auto-generated method stub
-//			String action = intent.getAction();
-//			if(Consts.UPDATE_CHANGE.equals(action)){
-//				
-////				Log.i("info", "nihao ");
-////				cartUtil = new CartUtil(getSherlockActivity(), layout, counTextView,
-////						sumTextView,mBox);
-////			
-////				cartUtil.AllComment();
-//			}
-//		}
-		
-//	}
-//	@Override
-//	public boolean onContextItemSelected(MenuItem item) {
-//		
-//		 switch (item.getItemId()) {
-//		 
-//		case MENU1://点击删除
-//			
-//			dataManage2 = new CartsDataManage();
-////			boolean isture = dataManage2.delCart(info.id+"");
-////			Log.i("info", isture+"   isture");
-//			break;
-//
-//		case MENU2://查看商品详情
-//			Intent intent = new Intent(getSherlockActivity(),GoodsInfoActivity.class);
-//			Bundle bundle = new Bundle();
-////			bundle.putSerializable("goodsId", info.id);
-//			intent.putExtras(bundle);
-//			getSherlockActivity().startActivity(intent);
-//			break;
-//		case MENU3://收藏
-//			Toast.makeText(getSherlockActivity(), "收藏成功", Toast.LENGTH_LONG).show();
-//			break;
-//		}
-//		
-//		
-//		return super.onContextItemSelected(item);
-//	}
+
+	// //接收广播
+	// private class InnerReceiver extends BroadcastReceiver{
+	//
+	// @Override
+	// public void onReceive(Context context, Intent intent) {
+	// // TODO Auto-generated method stub
+	// String action = intent.getAction();
+	// if(Consts.UPDATE_CHANGE.equals(action)){
+	//
+	// // Log.i("info", "nihao ");
+	// // cartUtil = new CartUtil(getSherlockActivity(), layout, counTextView,
+	// // sumTextView,mBox);
+	// //
+	// // cartUtil.AllComment();
+	// }
+	// }
+
+	// }
+	// @Override
+	// public boolean onContextItemSelected(MenuItem item) {
+	//
+	// switch (item.getItemId()) {
+	//
+	// case MENU1://点击删除
+	//
+	// dataManage2 = new CartsDataManage();
+	// // boolean isture = dataManage2.delCart(info.id+"");
+	// // Log.i("info", isture+"   isture");
+	// break;
+	//
+	// case MENU2://查看商品详情
+	// Intent intent = new
+	// Intent(getSherlockActivity(),GoodsInfoActivity.class);
+	// Bundle bundle = new Bundle();
+	// // bundle.putSerializable("goodsId", info.id);
+	// intent.putExtras(bundle);
+	// getSherlockActivity().startActivity(intent);
+	// break;
+	// case MENU3://收藏
+	// Toast.makeText(getSherlockActivity(), "收藏成功", Toast.LENGTH_LONG).show();
+	// break;
+	// }
+	//
+	//
+	// return super.onContextItemSelected(item);
+	// }
+
+	Addresses address = null;
+
+	/**
+	 * 判断是否用户已经有保存好的地址
+	 */
+	private void getAddresses() {
+
+		String userId = ((MyApplication) getSherlockActivity().getApplication())
+				.getUserId();
+		ArrayList<Addresses> mAddresses = addressManage.getAddressesArray(
+				Integer.parseInt(userId), 0, 5);
+		if (mAddresses.size() == 0) {
+			Intent intent = new Intent(getSherlockActivity(),
+					NewAddressActivity.class);
+			getSherlockActivity().startActivity(intent);
+			
+			// Log.i("info", "nihao");
+
+		} else {
+			address = mAddresses.remove(0);
+			// Log.i("info", address + "address");
+		}
+
+	}
 }
