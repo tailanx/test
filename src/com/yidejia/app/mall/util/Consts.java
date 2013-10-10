98cc5ffb22f7d82157b2dcda8f6252e27b741edf
liuyong <liyong@qq.com>
2013-10-10 10:09:55 +0800

    定义常量，用来发送通知，修改界面的购物车里面商品的数目

--- src/com/yidejia/app/mall/util/Consts.java
+++ src/com/yidejia/app/mall/util/Consts.java
@@ -2,4 +2,6 @@ package com.yidejia.app.mall.util;
 
 public class Consts {
 	public static final String UPDATE_CHANGE = "com.yidejia.UPDATE";
+	public static final int GENERAL = 126;
+	public static final int EMS = 127;
 }
