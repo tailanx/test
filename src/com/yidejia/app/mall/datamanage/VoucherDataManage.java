4354ac1eb8ed4f8793037be5f72906958493ae1f
longbin <675754509@qq.com>
2013-10-15 15:43:08 +0800

    补上积分的接口

--- src/com/yidejia/app/mall/datamanage/VoucherDataManage.java
+++ src/com/yidejia/app/mall/datamanage/VoucherDataManage.java
@@ -1,18 +1,97 @@
 package com.yidejia.app.mall.datamanage;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.yidejia.app.mall.net.voucher.Voucher;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
 /**
  * 获取用户积分
  * @author long bin
  *
  */
 public class VoucherDataManage {
+	
+	private String id;
+	private String token;
+	
+	private String voucherNum = "";
+	private String TAG = VoucherDataManage.class.getName();
+	private Context context;
+	
+	public VoucherDataManage(Context context){
+		this.context = context;
+	}
+	
 	/**
 	 * 获取用户积分
 	 * @param userid 用户id
 	 * @return
 	 */
-	public int getUserVoucher(String userid){
-		int voucher = -1;
-		return voucher;
+	public String getUserVoucher(String userid, String token){
+		this.id = userid;
+		this.token = token;
+		
+		boolean state = false;
+		
+		TaskVoucher taskVoucher = new TaskVoucher();
+		
+		try {
+			state = taskVoucher.execute().get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			Log.e(TAG, "task voucher InterruptedException");
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			Log.e(TAG, "task voucher ExecutionException");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			Toast.makeText(context, "网络不给力!", Toast.LENGTH_SHORT).show();
+		}
+		if(!state){
+			
+		}
+		return voucherNum;
 	};
+	
+	private class TaskVoucher extends AsyncTask<Void, Void, Boolean>{
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			Voucher voucher = new Voucher();
+			try {
+				String httpResponse = voucher.getHttpResponse(id, token);
+				try {
+					JSONObject httpObject = new JSONObject(httpResponse);
+					int code = httpObject.getInt("code");
+					if(code == 1){
+						String response = httpObject.getString("response");
+						JSONObject resObject = new JSONObject(response);
+						voucherNum = resObject.getString("can_use_score");
+						return true;
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return false;
+		}
+		
+	}
 }
