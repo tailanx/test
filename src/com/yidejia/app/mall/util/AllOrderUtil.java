606def53abc75d34abdde98ca2670d072d722b81
longbin <675754509@qq.com>
2013-11-05 09:30:30 +0800

    订单块公用这个

--- src/com/yidejia/app/mall/util/AllOrderUtil.java
+++ src/com/yidejia/app/mall/util/AllOrderUtil.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import com.yidejia.app.mall.MyApplication;
 import com.yidejia.app.mall.R;
 import com.yidejia.app.mall.datamanage.OrderDataManage;
+import com.yidejia.app.mall.model.Cart;
 import com.yidejia.app.mall.model.Order;
 import com.yidejia.app.mall.view.CstmPayActivity;
 import com.yidejia.app.mall.view.OrderDetailActivity;
@@ -62,22 +63,20 @@ public class AllOrderUtil {
 //
 //		}
 //	}
-/**
- * ������ͼ
- */
-	public void loadView(int fromIndex, int amount) {
+	
+	public void loadView(int orderTimeType, int orderType, int fromIndex, int amount) {
 		try {
 			orderDataManage = new OrderDataManage(context);
 			final ArrayList<Order> mList = orderDataManage.getOrderArray(
-					myApplication.getUserId(), "", "", "", fromIndex + "",
+					myApplication.getUserId(), "", String.valueOf(getOrderTime(orderTimeType)), getOrderTpye(orderType), fromIndex + "",
 					amount + "", myApplication.getToken());
 			// Log.i("info", mList.size()+"mList");
 			for (int i = 0; i < mList.size(); i++) {
 				
 				final View view = inflater.inflate(R.layout.all_order_item_item, null);
 				// Log.i("info", view+"");
-				Button mCancal = (Button) view.findViewById(R.id.all_order_item_main_cancal);
-				Button mButton = (Button) view
+				Button mCancalBtn = (Button) view.findViewById(R.id.all_order_item_main_cancal);
+				Button mOkBtn = (Button) view
 						.findViewById(R.id.all_order_item_main_pay);
 				final LinearLayout layout1= (LinearLayout) view.findViewById(R.id.all_order_item_main_linerar);
 				LinearLayout mLayout = (LinearLayout) view
@@ -90,10 +89,17 @@ public class AllOrderUtil {
 						.findViewById(R.id.all_order_item_main_sum_money_deatil);
 				TextView countTextView = (TextView) view
 						.findViewById(R.id.all_order_item_main_item_textview7_detail);
-
+				
+				
 				final Order mOrder = mList.get(i);
 				titleTextView.setText(mOrder.getStatus());
 				numberTextView.setText(mOrder.getOrderCode());
+				
+				mOrderType = getOrderTypeCode(mOrder.getStatus());
+				mOkBtn.setText(setOkBtnText(mOrderType));
+				if(mOrderType == 1 || mOrderType == 2) 
+					mCancalBtn.setText(setCancelBtnText(mOrderType));
+				else mCancalBtn.setVisibility(View.GONE);
 
 				final AllOrderDetail allOrderDetail = new AllOrderDetail(
 						context, mOrder, mLayout);
@@ -104,7 +110,7 @@ public class AllOrderUtil {
 					countTextView.setText(allOrderDetail.map.get("count")
 							.intValue() + "");
 				}
-				mCancal.setOnClickListener( new OnClickListener() {
+				mCancalBtn.setOnClickListener( new OnClickListener() {
 					
 					@Override
 					public void onClick(View v) {
@@ -114,31 +120,8 @@ public class AllOrderUtil {
 						mLinearLayoutLayout.removeView(layout1);
 					}
 				});
-				mButton.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						// TODO Auto-generated method stub
-//						Intent intent = new Intent(context,
-//								CstmPayActivity.class);
-//						Bundle mBundle = new Bundle();
-//						mBundle.putString("price",
-//								allOrderDetail.map.get("price") + "");
-//						intent.putExtra("carts", mOrder.getCartsArray());
-//						mBundle.putString("cartActivity", "No");
-//						intent.putExtras(mBundle);
-//						context.startActivity(intent);
-						Intent intent = new Intent(context,
-								OrderDetailActivity.class);
-
-						Bundle bundle = new Bundle();
-						bundle.putString("OrderCode", mOrder.getOrderCode());
-						bundle.putString("OrderPrice", allOrderDetail.map.get("price") + "");
-						intent.putExtras(bundle);
-						intent.putExtra("carts", mOrder.getCartsArray());
-						context.startActivity(intent);
-					}
-				});
+				mOkBtn.setOnClickListener(new OkClickListener(mOrder.getOrderCode(), allOrderDetail.map.get("price") + "", mOrder.getCartsArray()));
+				
 				mLayout.setOnClickListener(new OnClickListener() {
 					
 					@Override
@@ -168,5 +151,168 @@ public class AllOrderUtil {
 		}
 	}
 	
+	/**
+	 * 根据订单的状态（orderType）获取请求订单数据的字符串字段值
+	 * @param orderType（0-4分别表示全部订单，待付款订单，待发货订单，已发货订单，已完成订单）
+	 * @return
+	 */
+	private String getOrderTpye(int orderType){
+		String orderTypeStr = "";
+		switch (orderType) {
+		case 0:
+			orderTypeStr = "";
+			break;
+		case 1:
+			orderTypeStr = "录入";
+			break;
+		case 2:
+			orderTypeStr = "已付款";
+			break;
+		case 3:
+			orderTypeStr = "已发货";
+			break;
+		case 4:
+			orderTypeStr = "已签收";
+			break;
+
+		default:
+			break;
+		}
+		return orderTypeStr;
+	}
+	
+	/**
+	 * 根据订单时间状态（orderTimeType）获取订单的时间戳
+	 * @param orderTimeType 0-2 分别表示近一周，近一月，近一年
+	 * @return 近一周，近一月，近一年数据查询的时间戳
+	 */
+	private long getOrderTime(int orderTimeType){
+		long orderTime = System.currentTimeMillis()/1000;
+		switch (orderTimeType) {
+		case 0:
+			orderTime -= 7L * 24L * 60L * 60L;
+			break;
+		case 1:
+			orderTime -= 30L * 24L * 60L * 60L;
+			break;
+		case 2:
+			orderTime -= 365L * 24L * 60L * 60L;
+			break;
+
+		default:
+			break;
+		}
+		return orderTime;
+	}
+	
+	/**
+	 * 根据订单的状态（orderType）获取确定按钮的显示文字text
+	 * @param orderType 0-4分别表示全部订单，待付款订单，待发货订单，已发货订单，已完成订单
+	 * @return
+	 */
+	private String setOkBtnText(int orderType){
+		String text = "";
+		switch (orderType) {
+		case 0:
+			
+			break;
+		case 1:
+			text = "付款";
+			break;
+		case 2:
+			text = "催货";
+			break;
+		case 3:
+			text = "查看物流";
+			break;
+		case 4:
+			text = "评价";
+			break;
+
+		default:
+			break;
+		}
+		return text;
+	}
+	
+	/**
+	 * 根据订单的状态（orderType）获取取消按钮的显示文字text
+	 * @param orderType 0-4分别表示全部订单，待付款订单，待发货订单，已发货订单，已完成订单
+	 * @return
+	 */
+	private String setCancelBtnText(int orderType){
+		String text = "";
+		switch (orderType) {
+		case 0:
+			
+			break;
+		case 1:
+			text = "取消";
+			break;
+		case 2:
+			text = "查看";
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+			
+		default:
+			break;
+		}
+		return text;
+	}
+	
+	/**
+	 * 根据字符串获取订单的种类
+	 * @param str
+	 * @return
+	 */
+	private int getOrderTypeCode(String str){
+		int code = 0;
+		if("已付款".equals(str)) code = 2;
+		else if("录入".equals(str)) code = 1;
+		else if("已发货".equals(str)) code = 3;
+		else if("已签收".equals(str)) code = 4;
+		else code = 0;
+		return code;
+	}
+
+	
+	private int mOrderType;
+	private class OkClickListener implements View.OnClickListener{
+		
+		private String OrderPrice;
+		private String OrderCode;
+		private ArrayList<Cart> carts;
+		
+		public OkClickListener(String OrderCode, String OrderPrice, ArrayList<Cart> carts){
+			this.OrderCode = OrderCode;
+			this.OrderPrice = OrderPrice;
+			this.carts = carts;
+		}
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (mOrderType) {
+			case 1:
+				Intent intent = new Intent(context,
+						OrderDetailActivity.class);
+
+				Bundle bundle = new Bundle();
+				bundle.putString("OrderCode", OrderCode);//mOrder.getOrderCode()
+				bundle.putString("OrderPrice", OrderPrice);//allOrderDetail.map.get("price") + ""
+				intent.putExtras(bundle);
+				intent.putExtra("carts", carts);//mOrder.getCartsArray()
+				context.startActivity(intent);
+				break;
+
+			default:
+				break;
+			}
+		}
+		
+	};
 }
 
