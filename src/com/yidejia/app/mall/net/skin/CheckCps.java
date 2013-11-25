bbc3717ed3653e9fc73bd1ec8d2a649dc338c74f
longbin <675754509@qq.com>
2013-11-25 19:11:37 +0800

    添加检查cpsid的接口，

---
+++ src/com/yidejia/app/mall/net/skin/CheckCps.java
@@ -0,0 +1,46 @@
+package com.yidejia.app.mall.net.skin;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.yidejia.app.mall.jni.JNICallBack;
+import com.yidejia.app.mall.net.HttpGetConn;
+
+public class CheckCps {
+	
+	public String getHttpResp(String cpsid){
+		HttpGetConn conn = new HttpGetConn(new JNICallBack().getHttp4CheckCps(cpsid), true);
+		String httpresp = "";
+		try {
+			httpresp = conn.getJsonResult();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return httpresp;
+	}
+	
+	/**
+	 * 返回cps是否为正确的cps
+	 * @param httpresp
+	 * @return
+	 */
+	public boolean isCpsTrue(String httpresp){
+		try {
+			JSONObject jsonObject = new JSONObject(httpresp);
+			int code = jsonObject.optInt("code");
+			if(code == 1){
+				return true;
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
