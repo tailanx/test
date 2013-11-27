e17c8fa69609a37c6ba4a18fc8bf2dde14899375
longbin <675754509@qq.com>
2013-11-27 17:29:06 +0800

    去除了全屏

--- src/com/yidejia/app/mall/view/SkinQuesActivity.java
+++ src/com/yidejia/app/mall/view/SkinQuesActivity.java
@@ -70,8 +70,6 @@ public class SkinQuesActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		// getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
