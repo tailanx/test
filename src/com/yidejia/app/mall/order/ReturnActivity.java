494ecc0c20d8e3ed31ec12fd7bfb9b7e7e0a7ab6
longbin <675754509@qq.com>
2014-01-24 14:34:06 +0800

    修改申请退换货和退换货详情的方式

--- src/com/yidejia/app/mall/order/ReturnActivity.java
+++ src/com/yidejia/app/mall/order/ReturnActivity.java
@@ -1,26 +1,35 @@
 package com.yidejia.app.mall.order;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.baidu.mobstat.StatService;
+import com.opens.asyncokhttpclient.AsyncHttpResponse;
+import com.opens.asyncokhttpclient.AsyncOkHttpClient;
+import com.opens.asyncokhttpclient.RequestParams;
 import com.yidejia.app.mall.BaseActivity;
+import com.yidejia.app.mall.MyApplication;
 import com.yidejia.app.mall.R;
-import com.yidejia.app.mall.R.id;
-import com.yidejia.app.mall.R.layout;
-import com.yidejia.app.mall.R.string;
-import com.yidejia.app.mall.datamanage.TaskReturn;
+import com.yidejia.app.mall.jni.JNICallBack;
+import com.yidejia.app.mall.util.IsPhone;
 
 /**
- * 退换货详情
+ * 退换货详情或申请退换货
  * 
  * @author LongBin
  * 
@@ -33,25 +42,28 @@ public class ReturnActivity extends BaseActivity {
 	private TextView orderCodeTextView;
 	private TextView orderDateTextView;
 
-	private TaskReturn taskReturn;
 	private RetOrderInfo info;
-	private Button submit;
-	private EditText linkMan;
-	private EditText phoneNumber;
-	private Spinner reason;
-	private EditText describe;
-	private TextView exchange_title;
+	private EditText et_linkMan;
+	private EditText et_phoneNumber;
+	private Spinner sp_reason;
+	private EditText et_describe;
+	private TextView tv_exchange_title;
+	
+	private String cause;//退换原因
+	private String contact;//联系人
+	private String contact_manner;//联系方式
+	private String desc;//原因描述
+	private String userId;
+	private String token;	
+	private String theDate;	//申请退换货时间
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setActionbarConfig();
-		setTitle(getResources().getString(R.string.return_order_detail));
-		setContentView(R.layout.exchange);
 		TextView submit = (TextView) findViewById(R.id.ab_common_tv_right);
-		submit.setText(getResources().getString(R.string.commit));
-		submit.setVisibility(View.GONE);
-		submit.setOnClickListener(listener);
+		
+		setContentView(R.layout.exchange);
 
 		Bundle bundle = getIntent().getExtras();
 
@@ -60,37 +72,45 @@ public class ReturnActivity extends BaseActivity {
 		order_code = bundle.getString("orderCode");
 
 		the_date = bundle.getString("orderDate");
-		if (info == null || "".equals(info)) {
-			findIds();
+		
+		findIds();
+		
+		if (info == null || "".equals(info)) {	//申请退换货
+			setTitle(R.string.retrun_produce);
+			submit.setText(getResources().getString(R.string.commit));
+			submit.setVisibility(View.VISIBLE);
+			submit.setOnClickListener(listener);
 			orderCodeTextView.setText(order_code);
 			orderDateTextView.setText(the_date);
-			exchange_title.setText("退换货详情");
-		} else {
+			tv_exchange_title.setText(R.string.return_order_detail);
+		} else {	//退换货详情
 
+			setTitle(R.string.return_order_detail);
+			submit.setText(getResources().getString(R.string.commit));
+			submit.setVisibility(View.GONE);
 			// submit.setVisibility(View.GONE);
-			findIds();
 			getWindow().setSoftInputMode(
 					WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 			orderCodeTextView.setText(info.getOrderCode());
 
 			orderDateTextView.setText(info.getTheDate());
 
-			linkMan.setText(info.getContact());
-			linkMan.setEnabled(false);
-			linkMan.requestFocus();
+			et_linkMan.setText(info.getContact());
+			et_linkMan.setEnabled(false);
+			et_linkMan.requestFocus();
 
-			phoneNumber.setText(info.getContact_manner());
-			phoneNumber.setEnabled(false);
-			phoneNumber.requestFocus();
+			et_phoneNumber.setText(info.getContact_manner());
+			et_phoneNumber.setEnabled(false);
+			et_phoneNumber.requestFocus();
 
-			describe.setText(info.getDesc());
-			describe.setEnabled(false);
-			describe.requestFocus();
+			et_describe.setText(info.getDesc());
+			et_describe.setEnabled(false);
+			et_describe.requestFocus();
 
-			reason.setPrompt(info.getCause());
-			reason.setEnabled(false);
+			sp_reason.setPrompt(info.getCause());
+			sp_reason.setEnabled(false);
 
-			exchange_title.setText(info.getStatus());
+			tv_exchange_title.setText(info.getStatus());
 		}
 
 	}
@@ -98,18 +118,16 @@ public class ReturnActivity extends BaseActivity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		if (taskReturn != null)
-			taskReturn.closeTask();
 	}
 
 	private void findIds() {
 		orderCodeTextView = (TextView) findViewById(R.id.exchange_biaohao_number);
 		orderDateTextView = (TextView) findViewById(R.id.exchange_time_number);
-		linkMan = (EditText) findViewById(R.id.exchange_edittext_lianxiren);
-		phoneNumber = (EditText) findViewById(R.id.exchange_edittext_lianxifangshi);
-		reason = (Spinner) findViewById(R.id.exchange_spinner);
-		describe = (EditText) findViewById(R.id.go_pay_leave_message);
-		exchange_title = (TextView) findViewById(R.id.exchange_title);
+		et_linkMan = (EditText) findViewById(R.id.exchange_edittext_lianxiren);
+		et_phoneNumber = (EditText) findViewById(R.id.exchange_edittext_lianxifangshi);
+		sp_reason = (Spinner) findViewById(R.id.exchange_spinner);
+		et_describe = (EditText) findViewById(R.id.go_pay_leave_message);
+		tv_exchange_title = (TextView) findViewById(R.id.exchange_title);
 	}
 
 
@@ -120,23 +138,121 @@ public class ReturnActivity extends BaseActivity {
 
 		@Override
 		public void onClick(View v) {
-			taskReturn = new TaskReturn(ReturnActivity.this);
-			taskReturn.returnOrder(order_code);
+			
+			getParam();
 		}
 	};
+	
+	/**获取申请退换货的信息**/
+	@SuppressLint("SimpleDateFormat")
+	private void getParam(){
+		cause = sp_reason.getSelectedItem().toString().trim();
+		contact = et_linkMan.getText().toString().trim();
+		contact_manner = et_phoneNumber.getText().toString().trim();
+		desc = et_describe.getText().toString().trim();
+		if(TextUtils.isEmpty(cause)){
+			Toast.makeText(this, "请选择退换原因!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		if(TextUtils.isEmpty(contact)){
+			Toast.makeText(this, "联系人不能为空!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		if(TextUtils.isEmpty(contact_manner)){
+			Toast.makeText(this, "联系方式不能为空!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		if(TextUtils.isEmpty(desc)){
+			Toast.makeText(this, "原因描述不能为空!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		if(!IsPhone.isMobileNO(contact_manner)){
+			Toast.makeText(this,
+					getResources().getString(R.string.phone),
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		userId = MyApplication.getInstance().getUserId();
+		token = MyApplication.getInstance().getToken();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		theDate = sdf.format(new Date()).trim();
+		
+		commitReturn();
+	}
+	
+	/**提交申请**/
+	private void commitReturn(){
+		String param = new JNICallBack().getHttp4GetReturn(userId, order_code,
+				theDate, contact, contact_manner, cause, desc, token);
+		
+		String url = new JNICallBack().HTTPURL;
+		
+		RequestParams requestParams = new RequestParams();
+		requestParams.put(param);
+		
+		AsyncOkHttpClient client = new AsyncOkHttpClient();
+		client.post(url, requestParams, new AsyncHttpResponse(){
+
+			@Override
+			public void onStart() {
+				// TODO Auto-generated method stub
+				super.onStart();
+			}
+
+			@Override
+			public void onFinish() {
+				// TODO Auto-generated method stub
+				super.onFinish();
+			}
+
+			@Override
+			public void onSuccess(int statusCode, String content) {
+				super.onSuccess(statusCode, content);
+				if(HttpStatus.SC_OK == statusCode){
+					JSONObject jsonObject;
+					try {
+						jsonObject = new JSONObject(content);
+						int code = jsonObject.optInt("code");
+						if(code == 1){
+							String msg = jsonObject.optString("msg");
+							if("成功".equals(msg)) {
+								Toast.makeText(ReturnActivity.this, "提交成功!", Toast.LENGTH_LONG).show();
+								ReturnActivity.this.finish();
+								return;
+							} 
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+				Toast.makeText(ReturnActivity.this, "提交失败!", Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onError(Throwable error, String content) {
+				super.onError(error, content);
+				Toast.makeText(ReturnActivity.this, getString(R.string.bad_network), Toast.LENGTH_SHORT).show();
+			}
+			
+		});
+	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
 //		StatService.onResume(this);
-		StatService.onPageStart(this, "退换货详情页面");
+		if(info != null && !"".equals(info))
+			StatService.onPageStart(this, "退换货详情页面");
+		else StatService.onPageStart(this, getString(R.string.retrun_produce));
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 //		StatService.onPause(this);
-		StatService.onPageEnd(this, "退换货详情页面");
+		if(info != null && !"".equals(info))
+			StatService.onPageEnd(this, "退换货详情页面");
+		else StatService.onPageEnd(this, getString(R.string.retrun_produce));
 	}
 
 }
