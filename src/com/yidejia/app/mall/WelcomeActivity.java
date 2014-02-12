75e2ed1573fe5162b336dce0850c4b40e7f2db5a
longbin <675754509@qq.com>
2014-02-12 15:35:23 +0800

    修复分享sdk的混淆问题，打包带来的文件的变化

--- src/com/yidejia/app/mall/WelcomeActivity.java
+++ src/com/yidejia/app/mall/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
@@ -42,10 +43,9 @@ public class WelcomeActivity extends Activity {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_welcome);
 
-		// if((getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE) !=
-		// 0){
-		// android.os.Process.killProcess(android.os.Process.myPid());
-		// }
+		if ((getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+			android.os.Process.killProcess(android.os.Process.myPid());
+		}
 		StatService.setAppChannel(this, "", false);
 		StatService.setSessionTimeOut(30);
 		StatService.setLogSenderDelayed(10);
