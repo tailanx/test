0c2ad73a9f4088ecc811a9c86e3b128cfac7cead
longbin <675754509@qq.com>
2014-02-20 15:43:57 +0800

    修复伊日惠已结束活动显示数量的bug

--- src/com/yidejia/app/mall/yirihui/ParseYiRiHui.java
+++ src/com/yidejia/app/mall/yirihui/ParseYiRiHui.java
@@ -65,5 +65,56 @@ public class ParseYiRiHui {
 		return yiRiHuiDatas;
 	}
 	
-	
+	public boolean parseMainYRH(String content){
+		try {
+			JSONObject httpJsonObject = new JSONObject(content);
+			int code = httpJsonObject.optInt("code");
+			String strResp = httpJsonObject.optString("response");
+			long ts = httpJsonObject.optLong("ts");
+			if(1 == code){
+				
+				yiRiHuiDatas = new ArrayList<YiRiHuiData>();
+				YiRiHuiData tempYRH = null;
+//				JSONArray respArray = new JSONArray(strResp);
+//				int length = respArray.length();
+//				for (int i = 0; i < length; i++) {
+//					JSONObject yrhObject = respArray.optJSONObject(i);
+				JSONObject yrhObject = new JSONObject(strResp);
+				tempYRH = new YiRiHuiData();
+				tempYRH.setTheId(yrhObject.optString("the_id"));
+				tempYRH.setRuleName(yrhObject.optString("rule_name"));
+				tempYRH.setBeginTime(yrhObject.optLong("begin_time"));
+				long endTime = yrhObject.optLong("end_time");
+				// String strEndTime = yrhObject.optString("end_time");
+				tempYRH.setEndTime(endTime);
+				if ((endTime - ts) < 0)
+					tempYRH.setStartTime(0);
+				else
+					tempYRH.setStartTime((endTime - ts));
+
+				tempYRH.setGoodsId(yrhObject.optString("goods_id"));
+				tempYRH.setQuantity(yrhObject.optString("quantity"));
+				tempYRH.setCanBuyQuantity(yrhObject
+						.optString("can_buy_quantity"));
+				tempYRH.setOverTime(yrhObject.optString("overtime"));
+				tempYRH.setImg1(ImageUrl.IMAGEURL
+						+ yrhObject.optString("img_1") + "!150");
+				tempYRH.setImg2(ImageUrl.IMAGEURL
+						+ yrhObject.optString("img_2") + "!150");
+				tempYRH.setValidFlag(yrhObject.optString("valid_flag"));
+				tempYRH.setShellFlag(yrhObject.optString("shell_flag"));
+				tempYRH.setGoodsName(yrhObject.optString("goods_name"));
+				tempYRH.setGoodsPrice(yrhObject.optString("goods_price"));
+				yiRiHuiDatas.add(tempYRH);
+//				}
+				
+				return true;
+			} else if(-1 == code) {
+				return true;
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
--- src/com/yidejia/app/mall/yirihui/YiRiHuiAdapter.java
+++ src/com/yidejia/app/mall/yirihui/YiRiHuiAdapter.java
@@ -129,6 +129,17 @@ public class YiRiHuiAdapter extends BaseAdapter{
 		String imgUrlSmall = yiRiHuiDatas.get(position).getImg2();
 		ImageLoader.getInstance().displayImage(imgUrlSmall, holder.ivGoodsSmall, options, animateFirstListener);
 		
+
+		try {
+			final int canBuyCount = Integer.parseInt(totalCount);
+			if(canBuyCount <= 0) {	//用户可购买的数量小于0
+				holder.tvBuyNow.setSelected(false);
+				holder.tvCount.setSelected(false);
+			}
+			if(canBuyCount < 10) holder.tvCount.setText("0" + canBuyCount);
+		} catch (NumberFormatException e) {
+		}
+		
 		//已结束 马上购买文字变为已售罄
 		if(-1 == type) {
 			holder.tvBuyNow.setText("已售罄");
@@ -142,26 +153,13 @@ public class YiRiHuiAdapter extends BaseAdapter{
 				
 				@Override
 				public void onTick(long millisUntilFinished) {
-					// TODO Auto-generated method stub
 					setTime(holder, millisUntilFinished / 1000);
 				}
 				
 				@Override
 				public void onFinish() {
-					// TODO Auto-generated method stub
-					
 				}
 			}.start();
-		}
-		
-		try {
-			final int canBuyCount = Integer.parseInt(totalCount);
-			if(canBuyCount <= 0) {	//用户可购买的数量小于0
-				holder.tvBuyNow.setSelected(false);
-				holder.tvCount.setSelected(false);
-			}
-			if(canBuyCount < 10) holder.tvCount.setText("0" + canBuyCount);
-		} catch (NumberFormatException e) {
 		}
 		
 		if(0 == type) {
